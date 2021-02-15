5c317f3d19af4a51aebc8546a71501beaffe849c
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-02-15 15:41:20 +0100

    Creata una classe con i suoi attributi e creatoil constructor
    Creata una classe con i suoi attributi e creatoil constructor

--- index.php
+++ index.php
@@ -3,4 +3,32 @@
 // Create una classe per descrivere un entitá, assegnatele degli attributi, utilizzate il constructor. 
 //Create alcune istanze della classe. Stampate a schermo gli attributi di ogni istanza.
 
+
+/** class Cane
+ * @author Marco
+ * @copyright 2021 Marco
+ */
+
+
+ //classe
+ class Cane {
+    //attributi
+    public $nome;
+    public $paese;
+    public $taglia;
+    public $aspettative_di_vita_media;
+    public $gruppo;
+    public $caratteristiche;
+
+    //constructor
+    function __construct(string $nome, string $paese, string $taglia, string $aspettative_di_vita_media, string $gruppo, string $caratteristiche){
+        $this->nome = $nome;
+        $this->paese = $paese;
+        $this->taglia = $taglia;
+        $this->aspettative_di_vita_media = $aspettative_di_vita_media;
+        $this->gruppo = $gruppo;
+        $this->caratteristiche = $caratteristiche;
+    }
+ }
+
 ?>
